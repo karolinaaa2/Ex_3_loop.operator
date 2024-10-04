3b95cef9b0a93479c1fab6d009e0027c35adc665
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:50:39 +0300

    Add project files.

---
+++ Ex_3_loop.operator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число для таблиці множення: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Таблиця множення для {number}:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} * {i} = {number * i}");
+        }
+    }
+}
+
